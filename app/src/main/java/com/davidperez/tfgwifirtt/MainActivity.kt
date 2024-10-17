8b3cca54cc8d220174f828d322b81e7dea649ba8
dACEvid <dperezsenosiain@gmail.com>
2024-10-17 22:27:05 +0200

    add logic for access point scanning

--- app/src/main/java/com/davidperez/tfgwifirtt/MainActivity.kt
+++ app/src/main/java/com/davidperez/tfgwifirtt/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.davidperez.tfgwifirtt
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
 import com.davidperez.tfgwifirtt.ui.theme.TFGWiFiRTTTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var wifiManager: WifiManager
+    lateinit var locationManager: LocationManager
+    //var scanResultList = ArrayList<ScanResult>()
+    var scanResultList = mutableListOf<ScanResult>()
+
+    //val intentFilter = IntentFilter()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize  WiFiManager and LocationManager
+        //wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Register a broadcast listener for SCAN_RESULTS_AVAILABLE_ACTION, which is called when scan requests are completed
+        //intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        //this.applicationContext.registerReceiver(wifiScanReceiver, intentFilter)
+        //registerReceiver(wifiScanReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+
+        requestNeededPermissions()
+
+        startScanning()
+
         setContent {
             TFGWiFiRTTTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +67,98 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingText(
+                        message = "Test message",
+                        from = "From David",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
     }
+
+    private fun startScanning() {
+        val wifiScanReceiver = object : BroadcastReceiver() {
+            @SuppressLint("MissingPermission") // Permissions are checked before starting to scan
+            override fun onReceive(context: Context, intent: Intent) {
+                Log.d("Test", "onReceive Called")
+                val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+                if (!success) {
+                    Log.e("TestDavid", "Scanning of Access Points failed1")
+                }
+
+                scanResultList = wifiManager.scanResults
+                Log.d("TestDavid", scanResultList.joinToString())
+            }
+        }
+
+        // Register a broadcast listener for SCAN_RESULTS_AVAILABLE_ACTION, which is called when scan requests are completed
+        registerReceiver(wifiScanReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            Log.e("TestDavid", "Scanning of Access Points failed2")
+        }
+    }
+
+
+    private fun requestNeededPermissions() {
+        // check if location is enabled
+        if (!locationManager.isLocationEnabled) {
+            Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show()
+        }
+
+        // Permissions needed for a successful call to startScan()
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+        }
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+        }
+        if (checkSelfPermission(Manifest.permission.CHANGE_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CHANGE_WIFI_STATE), 1)
+        }
+
+        // Permission needed for a successful call to getScanResults()
+        if (checkSelfPermission(Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_WIFI_STATE), 1)
+        }
+    }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+
+@Composable
+fun AccessPointList() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     TFGWiFiRTTTheme {
-        Greeting("Android")
+        GreetingText(message = "Test message!", from = "From David")
     }
 }
