4634ddba3545df41018fe7eff305cfccce2e7474
dACEvid <dperezsenosiain@gmail.com>
2024-12-24 16:24:51 +0100

    show information about the number of APs discovered

--- app/src/main/java/com/davidperez/tfgwifirtt/ui/components/TitleWithDivider.kt
+++ app/src/main/java/com/davidperez/tfgwifirtt/ui/components/TitleWithDivider.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,19 +18,20 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun TitleWithDivider(
     title: String,
+    isSubtitle: Boolean = false
 ) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.primary)
+            .background(if (isSubtitle) MaterialTheme.colorScheme.inversePrimary else MaterialTheme.colorScheme.primary)
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = title,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.onPrimary
+            fontSize = if (isSubtitle) 12.sp else 30.sp,
+            fontWeight = if (isSubtitle) FontWeight.Normal else FontWeight.Bold,
+            color = if (isSubtitle) Color.Gray else MaterialTheme.colorScheme.onPrimary
         )
         Divider(
             modifier = Modifier
--- app/src/main/java/com/davidperez/tfgwifirtt/ui/screens/AccessPointsScreen.kt
+++ app/src/main/java/com/davidperez/tfgwifirtt/ui/screens/AccessPointsScreen.kt
@@ -5,6 +5,7 @@ import android.net.wifi.rtt.RangingResult
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,12 +15,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Switch
@@ -30,6 +34,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
@@ -95,17 +100,20 @@ private fun AccessPoints(
     )
 
     var accessPointsToShow: List<AccessPoint> = accessPointsList
+    val rttCompatibleAccessPoints = accessPointsList.filter { it.isWifiRTTCompatible }
+
     if (userSettings.showOnlyRttCompatibleAps ) {
-        accessPointsToShow = accessPointsList.filter { it.isWifiRTTCompatible }
+        accessPointsToShow = rttCompatibleAccessPoints
     }
 
     // Show access point list
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        if (accessPointsToShow.isNotEmpty()) {
+        if (accessPointsList.isNotEmpty()) {
             stickyHeader {
                 TitleWithDivider("Visible Access Points")
+                TitleWithDivider("${accessPointsList.size} APs discovered (${rttCompatibleAccessPoints.size} of them are RTT-capable)", true)
             }
         }
         items(accessPointsToShow) { ap ->
